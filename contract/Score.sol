7814203fa47bc8ed0404f1970a5a96b9461f3a0b
陈宇峰 <yufengcode@gmail.com>
2017-01-03 23:23:24 +0800

    使用geth动态创建一个新账户

--- contract/Score.sol
+++ contract/Score.sol
@@ -53,6 +53,7 @@ contract Score is Utils, Test {
 
     struct Customer {
         address customerAddr; //客户address
+        bytes32 phone; //客户手机
         bytes32 password; //客户密码
         uint scoreAmount; //积分余额
         bytes32[] buyGoods; //购买的商品数组
@@ -72,10 +73,12 @@ contract Score is Utils, Test {
     }
 
     mapping (address=>Customer) customer; 
+    mapping (bytes32=>address) customerPhone;
     mapping (address=>Merchant) merchant; 
     mapping (bytes32=>Good) good; //根据商品Id查找该件商品
 
-    address[] customers; //已注册的客户数组
+    address[] customerAddrs; //已注册的客户地址数组
+    bytes32[] customerPhones; //已注册的客户手机数组
     address[] merchants; //已注册的商户数组
     bytes32[] goods; //已经上线的商品数组
 
@@ -97,14 +100,19 @@ contract Score is Utils, Test {
 
     //注册一个客户
     event RegisterCustomer(address sender, string message);
-    function registerCustomer(address _customerAddr, string _password) {
-
+    function registerCustomer(address _customerAddr, 
+        string _phone, 
+        string _password) {
         //判断是否已经注册
-        if(!isCustomerAlreadyRegister(_customerAddr)) {
+        if(!isCustomerAlreadyRegister(_phone)) {
             //还未注册
             customer[_customerAddr].customerAddr = _customerAddr;
+            customer[_customerAddr].phone = stringToBytes32(_phone);
             customer[_customerAddr].password = stringToBytes32(_password);
-            customers.push(_customerAddr);
+
+            customerPhone[stringToBytes32(_phone)] = _customerAddr;
+            customerAddrs.push(_customerAddr);
+            customerPhones.push(stringToBytes32(_phone));
             RegisterCustomer(msg.sender, "注册成功");
             return;
         }
@@ -115,27 +123,27 @@ contract Score is Utils, Test {
     }
 
     //注册一个商户
-    event NewMerchant(address sender, bool isSuccess, string message);
-    function newMerchant(address _merchantAddr) {
+    // event NewMerchant(address sender, bool isSuccess, string message);
+    // function newMerchant(address _merchantAddr) {
 
-        //判断是否已经注册
-        if(!isMerchantAlreadyRegister(_merchantAddr)) {
-            //还未注册
-            merchant[_merchantAddr].merchantAddr = _merchantAddr;
-            merchants.push(_merchantAddr);
-            NewMerchant(msg.sender, true, "注册成功");
-            return;
-        }
-        else {
-            NewMerchant(msg.sender, false, "该账户已经注册");
-            return;
-        }
-    }
+    //     //判断是否已经注册
+    //     if(!isMerchantAlreadyRegister(_merchantAddr)) {
+    //         //还未注册
+    //         merchant[_merchantAddr].merchantAddr = _merchantAddr;
+    //         merchants.push(_merchantAddr);
+    //         NewMerchant(msg.sender, true, "注册成功");
+    //         return;
+    //     }
+    //     else {
+    //         NewMerchant(msg.sender, false, "该账户已经注册");
+    //         return;
+    //     }
+    // }
 
     //判断一个客户是否已经注册
-    function isCustomerAlreadyRegister(address _customerAddr)internal returns(bool) {
-        for(uint i = 0; i < customers.length; i++) {
-            if(customers[i] == _customerAddr) {
+    function isCustomerAlreadyRegister(string _phone)internal returns(bool) {
+        for(uint i = 0; i < customerPhones.length; i++) {
+            if(customerPhones[i] == stringToBytes32(_phone)) {
                 return true;
             }
         }
@@ -143,211 +151,211 @@ contract Score is Utils, Test {
     }
 
     //判断一个商户是否已经注册
-    function isMerchantAlreadyRegister(address _merchantAddr)internal returns(bool) {
-        for(uint i = 0; i < merchants.length; i++) {
-            if(merchants[i] == _merchantAddr) {
-                return true;
-            }
-        }
-        return false;
-    }
+    // function isMerchantAlreadyRegister(address _merchantAddr)internal returns(bool) {
+    //     for(uint i = 0; i < merchants.length; i++) {
+    //         if(merchants[i] == _merchantAddr) {
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
 
     //设置商户密码
-    event SetMerchantPassword(address sender, string message);
-    function setMerchantPassword(address _merchantAddr, string _password) {
-        merchant[_merchantAddr].password = stringToBytes32(_password);
-        SetMerchantPassword(msg.sender, "设置密码成功");
-    }
+    // event SetMerchantPassword(address sender, string message);
+    // function setMerchantPassword(address _merchantAddr, string _password) {
+    //     merchant[_merchantAddr].password = stringToBytes32(_password);
+    //     SetMerchantPassword(msg.sender, "设置密码成功");
+    // }
 
     //查询用户密码
-    function getCustomerPassword(address _customerAddr)constant returns(bool, bytes32) {
-        //先判断该用户是否注册
-        if(isCustomerAlreadyRegister(_customerAddr)) {
-            return (true, customer[_customerAddr].password);
-        }
-        else {
-            return(false, "");
-        }
-    }
+    // function getCustomerPassword(address _customerAddr)constant returns(bool, bytes32) {
+    //     //先判断该用户是否注册
+    //     if(isCustomerAlreadyRegister(_customerAddr)) {
+    //         return (true, customer[_customerAddr].password);
+    //     }
+    //     else {
+    //         return(false, "");
+    //     }
+    // }
 
     //查询商户密码
-    function getMerchantPassword(address _merchantAddr)constant returns(bool, bytes32) {
-        //先判断该商户是否注册
-        if(isMerchantAlreadyRegister(_merchantAddr)) {
-            return (true, merchant[_merchantAddr].password);
-        }
-        else {
-            return(false, "");
-        }
-    }
+    // function getMerchantPassword(address _merchantAddr)constant returns(bool, bytes32) {
+    //     //先判断该商户是否注册
+    //     if(isMerchantAlreadyRegister(_merchantAddr)) {
+    //         return (true, merchant[_merchantAddr].password);
+    //     }
+    //     else {
+    //         return(false, "");
+    //     }
+    // }
 
     //银行发送积分给客户,只能被银行调用，且只能发送给客户
-    event SendScoreToCustomer(address sender, string message);
-    function sendScoreToCustomer(address _receiver, 
-        uint _amount)onlyOwner {
+    // event SendScoreToCustomer(address sender, string message);
+    // function sendScoreToCustomer(address _receiver, 
+    //     uint _amount)onlyOwner {
 
-        if(isCustomerAlreadyRegister(_receiver)) {
-            //已经注册
-            issuedScoreAmount += _amount;
-            customer[_receiver].scoreAmount += _amount;
-            SendScoreToCustomer(msg.sender, "发行积分成功");
-            return;
-        }
-        else {
-            //还没注册
-            SendScoreToCustomer(msg.sender, "该账户未注册，发行积分失败");
-            return;
-        }
-    }
+    //     if(isCustomerAlreadyRegister(_receiver)) {
+    //         //已经注册
+    //         issuedScoreAmount += _amount;
+    //         customer[_receiver].scoreAmount += _amount;
+    //         SendScoreToCustomer(msg.sender, "发行积分成功");
+    //         return;
+    //     }
+    //     else {
+    //         //还没注册
+    //         SendScoreToCustomer(msg.sender, "该账户未注册，发行积分失败");
+    //         return;
+    //     }
+    // }
 
     //根据客户address查找余额
-    function getScoreWithCustomerAddr(address customerAddr)constant returns(uint) {
-        return customer[customerAddr].scoreAmount;
-    }
+    // function getScoreWithCustomerAddr(address customerAddr)constant returns(uint) {
+    //     return customer[customerAddr].scoreAmount;
+    // }
 
     //根据商户address查找余额
-    function getScoreWithMerchantAddr(address merchantAddr)constant returns(uint) {
-        return merchant[merchantAddr].scoreAmount;
-    }
+    // function getScoreWithMerchantAddr(address merchantAddr)constant returns(uint) {
+    //     return merchant[merchantAddr].scoreAmount;
+    // }
 
     //两个账户转移积分，任意两个账户之间都可以转移，客户商户都调用该方法
     //_senderType表示调用者类型，0表示客户，1表示商户
-    event TransferScoreToAnother(address sender, string message);
-    function transferScoreToAnother(uint _senderType, 
-        address _sender, 
-        address _receiver, 
-        uint _amount) {
-        string memory message;
-        if(!isCustomerAlreadyRegister(_receiver) && !isMerchantAlreadyRegister(_receiver)) {
-            //目的账户不存在
-            TransferScoreToAnother(msg.sender, "目的账户不存在，请确认后再转移！");
-            return;
-        }
-        if(_senderType == 0) {
-            //客户转移
-            if(customer[_sender].scoreAmount >= _amount) {
-                customer[_sender].scoreAmount -= _amount;
+    // event TransferScoreToAnother(address sender, string message);
+    // function transferScoreToAnother(uint _senderType, 
+    //     address _sender, 
+    //     address _receiver, 
+    //     uint _amount) {
+    //     string memory message;
+    //     if(!isCustomerAlreadyRegister(_receiver) && !isMerchantAlreadyRegister(_receiver)) {
+    //         //目的账户不存在
+    //         TransferScoreToAnother(msg.sender, "目的账户不存在，请确认后再转移！");
+    //         return;
+    //     }
+    //     if(_senderType == 0) {
+    //         //客户转移
+    //         if(customer[_sender].scoreAmount >= _amount) {
+    //             customer[_sender].scoreAmount -= _amount;
         
-                if(isCustomerAlreadyRegister(_receiver)) {
-                    //目的地址是客户
-                    customer[_receiver].scoreAmount += _amount;
-                }else {
-                    merchant[_receiver].scoreAmount += _amount;
-                }
-                TransferScoreToAnother(msg.sender, "积分转让成功！");
-                return;
-          }else {
-                TransferScoreToAnother(msg.sender, "你的积分余额不足，转让失败！");
-                return;
-          }
-        }else {
-            //商户转移
-            if(merchant[_sender].scoreAmount >= _amount) {
-                merchant[_sender].scoreAmount -= _amount;
-                if(isCustomerAlreadyRegister(_receiver)) {
-                    //目的地址是客户
-                    customer[_receiver].scoreAmount += _amount;
-                }else {
-                    merchant[_receiver].scoreAmount += _amount;
-                }
-                TransferScoreToAnother(msg.sender, "积分转让成功！");
-                return;
-            }else {
-                 TransferScoreToAnother(msg.sender, "你的积分余额不足，转让失败！");
-                 return;
-            }
-        }
-    }
+    //             if(isCustomerAlreadyRegister(_receiver)) {
+    //                 //目的地址是客户
+    //                 customer[_receiver].scoreAmount += _amount;
+    //             }else {
+    //                 merchant[_receiver].scoreAmount += _amount;
+    //             }
+    //             TransferScoreToAnother(msg.sender, "积分转让成功！");
+    //             return;
+    //       }else {
+    //             TransferScoreToAnother(msg.sender, "你的积分余额不足，转让失败！");
+    //             return;
+    //       }
+    //     }else {
+    //         //商户转移
+    //         if(merchant[_sender].scoreAmount >= _amount) {
+    //             merchant[_sender].scoreAmount -= _amount;
+    //             if(isCustomerAlreadyRegister(_receiver)) {
+    //                 //目的地址是客户
+    //                 customer[_receiver].scoreAmount += _amount;
+    //             }else {
+    //                 merchant[_receiver].scoreAmount += _amount;
+    //             }
+    //             TransferScoreToAnother(msg.sender, "积分转让成功！");
+    //             return;
+    //         }else {
+    //              TransferScoreToAnother(msg.sender, "你的积分余额不足，转让失败！");
+    //              return;
+    //         }
+    //     }
+    // }
 
     //银行查找已经发行的积分总数
-    function getIssuedScoreAmount()constant returns(uint) {
-        return issuedScoreAmount;
-    }
+    // function getIssuedScoreAmount()constant returns(uint) {
+    //     return issuedScoreAmount;
+    // }
 
     //（1）商户添加一件商品:（1）（2）（3）方法分拆解决out of gas
-    event AddGood(address sender, bool isSuccess, string message);
-    function addGood(address _merchantAddr, string _goodId, uint _price) {
-        bytes32 tempId = stringToBytes32(_goodId);
+    // event AddGood(address sender, bool isSuccess, string message);
+    // function addGood(address _merchantAddr, string _goodId, uint _price) {
+    //     bytes32 tempId = stringToBytes32(_goodId);
 
-        //首先判断该商品Id是否已经存在
-        if(!isGoodAlreadyAdd(tempId)) {
-            good[tempId].goodId = tempId;
-            good[tempId].price = _price;
-            good[tempId].belong = _merchantAddr;
-            AddGood(msg.sender, true, "创建商品成功");
-            return;
-        }
-        else {
-            AddGood(msg.sender, false, "该件商品已经添加，请确认后操作");
-            return;
-        }
-    }
+    //     //首先判断该商品Id是否已经存在
+    //     if(!isGoodAlreadyAdd(tempId)) {
+    //         good[tempId].goodId = tempId;
+    //         good[tempId].price = _price;
+    //         good[tempId].belong = _merchantAddr;
+    //         AddGood(msg.sender, true, "创建商品成功");
+    //         return;
+    //     }
+    //     else {
+    //         AddGood(msg.sender, false, "该件商品已经添加，请确认后操作");
+    //         return;
+    //     }
+    // }
 
     //（2）商户添加一件商品
-    event PutGoodToArray(address sender, string message);
-    function putGoodToArray(string _goodId) {
-        goods.push(stringToBytes32(_goodId));  
-        PutGoodToArray(msg.sender, "添加到全局商品数组成功");
-    }
+    // event PutGoodToArray(address sender, string message);
+    // function putGoodToArray(string _goodId) {
+    //     goods.push(stringToBytes32(_goodId));  
+    //     PutGoodToArray(msg.sender, "添加到全局商品数组成功");
+    // }
 
     //（3）商户添加一件商品
-    event PutGoodToMerchant(address sender, string message);
-    function putGoodToMerchant(address _merchantAddr, string _goodId) {
-         merchant[_merchantAddr].sellGoods.push(stringToBytes32(_goodId)); 
-         PutGoodToMerchant(msg.sender, "添加到商户的商品数组成功");
-    }
+    // event PutGoodToMerchant(address sender, string message);
+    // function putGoodToMerchant(address _merchantAddr, string _goodId) {
+    //      merchant[_merchantAddr].sellGoods.push(stringToBytes32(_goodId)); 
+    //      PutGoodToMerchant(msg.sender, "添加到商户的商品数组成功");
+    // }
 
     //商户查找自己的商品数组
-    function getGoodsByMerchant(address _merchantAddr)constant returns(bytes32[]) {
-        return merchant[_merchantAddr].sellGoods;
-    }
+    // function getGoodsByMerchant(address _merchantAddr)constant returns(bytes32[]) {
+    //     return merchant[_merchantAddr].sellGoods;
+    // }
 
     //（1）用户用积分购买一件商品,拆分方法，解决out of gas
-    event BuyGood(address sender, bool isSuccess, string message);
-    function buyGood(address _customerAddr, string _goodId) {
-        //首先判断输入的商品Id是否存在
-        bytes32 tempId = stringToBytes32(_goodId);
-        if(isGoodAlreadyAdd(tempId)) {
-            //该件商品已经添加，可以购买
-            if(customer[_customerAddr].scoreAmount < good[tempId].price) {
-                BuyGood(msg.sender, false, "余额不足，购买商品失败");
-                return;
-            }
-            else {
-                //对这里的方法抽取      
-                BuyGood(msg.sender, true, "购买商品成功");
-                return;
-            }
-        }
-        else {
-            //没有这个Id的商品
-            BuyGood(msg.sender, false, "输入商品Id不存在，请确定后购买");
-            return;
-        }
-    }
+    // event BuyGood(address sender, bool isSuccess, string message);
+    // function buyGood(address _customerAddr, string _goodId) {
+    //     //首先判断输入的商品Id是否存在
+    //     bytes32 tempId = stringToBytes32(_goodId);
+    //     if(isGoodAlreadyAdd(tempId)) {
+    //         //该件商品已经添加，可以购买
+    //         if(customer[_customerAddr].scoreAmount < good[tempId].price) {
+    //             BuyGood(msg.sender, false, "余额不足，购买商品失败");
+    //             return;
+    //         }
+    //         else {
+    //             //对这里的方法抽取      
+    //             BuyGood(msg.sender, true, "购买商品成功");
+    //             return;
+    //         }
+    //     }
+    //     else {
+    //         //没有这个Id的商品
+    //         BuyGood(msg.sender, false, "输入商品Id不存在，请确定后购买");
+    //         return;
+    //     }
+    // }
 
     //（2）对上面buyGood()方法的拆分
-    function buyGoodSuccess(address _customerAddr, string _goodId) {
-         bytes32 tempId = stringToBytes32(_goodId);
-         customer[_customerAddr].scoreAmount -= good[tempId].price;
-         merchant[good[tempId].belong].scoreAmount += good[tempId].price;
-         customer[_customerAddr].buyGoods.push(tempId);
-    }
+    // function buyGoodSuccess(address _customerAddr, string _goodId) {
+    //      bytes32 tempId = stringToBytes32(_goodId);
+    //      customer[_customerAddr].scoreAmount -= good[tempId].price;
+    //      merchant[good[tempId].belong].scoreAmount += good[tempId].price;
+    //      customer[_customerAddr].buyGoods.push(tempId);
+    // }
 
     //客户查找自己的商品数组
-    function getGoodsByCustomer(address _customerAddr)constant returns(bytes32[]) {
-        return customer[_customerAddr].buyGoods;
-    }
+    // function getGoodsByCustomer(address _customerAddr)constant returns(bytes32[]) {
+    //     return customer[_customerAddr].buyGoods;
+    // }
 
     //首先判断输入的商品Id是否存在
-    function isGoodAlreadyAdd(bytes32 _goodId)internal returns(bool) {
-        for(uint i = 0; i < goods.length; i++) {
-            if(goods[i] == _goodId) {
-                return true;
-            }
-        }
-        return false;
-    }
+    // function isGoodAlreadyAdd(bytes32 _goodId)internal returns(bool) {
+    //     for(uint i = 0; i < goods.length; i++) {
+    //         if(goods[i] == _goodId) {
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
 }
 
 

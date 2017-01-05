7993dd3cffc8c9178d715d2508a33c1116fc43a8
陈宇峰 <yufengcode@gmail.com>
2017-01-06 01:05:26 +0800

    使用secp256k1生成公私钥对，优化创建秘钥对

--- public/javascripts/utils/generateKey.js
+++ public/javascripts/utils/generateKey.js
@@ -1,40 +1,34 @@
 var crypto = require('crypto');
+var ethereumjs_util = require('ethereumjs-util');
 
 /**
- * 曲线类型：
- * sect163k1 (1), sect163r1 (2),sect163r2 (3),
-
+ 曲线类型：
+ sect163k1 (1), sect163r1 (2),sect163r2 (3),
  sect193r1 (4), sect193r2 (5),sect233k1 (6),
-
  sect233r1 (7), sect239k1 (8),sect283k1 (9),
-
  sect283r1 (10), sect409k1 (11),sect409r1 (12),
-
  sect571k1 (13), sect571r1 (14),secp160k1 (15),
-
  secp160r1 (16), secp160r2 (17),secp192k1 (18),
-
  secp192r1 (19), secp224k1 (20),secp224r1 (21),
-
  secp256k1 (22), secp256r1 (23),secp384r1 (24),
-
  secp521r1 (25),
  *
+ * 以太坊中是默认使用secp256k1
  */
 
 function generateKeys() {
-    var ecdh = crypto.createECDH("secp256k1");
-    ecdh.generateKeys('hex');
-    var publicKey = ecdh.getPublicKey('hex');
-    var privateKey = ecdh.getPrivateKey('hex');
-    var accountAddress = "0x" + publicKey.substr(0, 40);
-    console.log("公钥：" + publicKey + ";长度: " + publicKey.length);
-    console.log("私钥：" + privateKey + "；长度：" + privateKey.length);
-    console.log("账户地址：" + accountAddress + "; 长度：" + accountAddress.length);
+    //公私钥对需要保存在数据库中
+    var privateKey = crypto.randomBytes(32);
+    var publicKey = ethereumjs_util.privateToPublic(privateKey);
+    var accountAddress = ethereumjs_util.privateToAddress(privateKey);
+    console.log("公钥：" + ethereumjs_util.bufferToHex(publicKey));
+    console.log("私钥: " + ethereumjs_util.bufferToHex(privateKey));
+    console.log("公私钥对计算出账号：" + ethereumjs_util.bufferToHex(accountAddress));
+
     var keys = {
-        publicKey: publicKey,
-        privateKey: privateKey,
-        accountAddress: accountAddress
+        publicKey: ethereumjs_util.bufferToHex(publicKey),
+        privateKey: ethereumjs_util.bufferToHex(privateKey),
+        accountAddress: ethereumjs_util.bufferToHex(accountAddress)
     };
     return keys;
 }

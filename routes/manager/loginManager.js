7928f94a687c4cb2eecbed3d79f247001d2832b2
陈宇峰 <yufengcode@gmail.com>
2017-01-07 22:11:28 +0800

    实现银行管理员登录接口

---
+++ routes/manager/loginManager.js
@@ -0,0 +1,51 @@
+//处理客户登录的路由
+var express = require('express');
+var web3Instance = require('../../public/javascripts/utils/ethereumUtils/web3Instance');
+
+//web3初始化
+var web3 = web3Instance.web3;
+
+/**
+ * 返回码说明：
+ * 0：成功；
+ * 1：失败；
+ *
+ * @param req
+ * phone:手机号码；
+ * password:密码；
+ *
+ * @param res
+ * code:状态码
+ * message:消息
+ * txInfo:区块链交易信息
+ */
+module.exports.login = function (req, res){
+
+    console.log("请求参数：" + req.query.phone + "    " + req.query.password);
+    global.contractInstance.loginManager(req.query.phone, req.query.password, {from: web3.eth.accounts[0]}, function (error, result) {
+        if (!error) {
+            var eventLoginManager = global.contractInstance.LoginManager();
+            eventLoginManager.watch(function (error, result) {
+                console.log("状态码：" + result.args.statusCode + "消息：" + result.args.message);
+                var response = {
+                    code: result.args.statusCode,
+                    message: result.args.message,
+                    txInfo: result
+                };
+                eventLoginManager.stopWatching();
+                res.send(JSON.stringify(response));
+                res.end();
+            });
+        }
+        else {
+            console.log("发生错误：" + error);
+            var response = {
+                code: 1,
+                message: error.toString(),
+                txInfo: ""
+            };
+            res.send(JSON.stringify(response));
+            res.end();
+        }
+    });
+};

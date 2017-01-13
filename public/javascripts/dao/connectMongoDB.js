20e9dae41650f6af964d1116576a06d93e978951
陈宇峰 <yufengcode@gmail.com>
2017-01-13 10:54:40 +0800

    完成mongodb测试代码

---
+++ public/javascripts/dao/connectMongoDB.js
@@ -0,0 +1,12 @@
+/**
+ * Created by chenyufeng on 13/01/2017.
+ */
+var mongoose = require('mongoose');
+var configDAO = require('./configDAO');
+
+module.exports.connect = function () {
+     var db = mongoose.connect(configDAO.mongodb);
+     //这里加载模型
+    require('./model/customer.server.model');
+    return db;
+};

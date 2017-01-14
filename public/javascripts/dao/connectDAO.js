2c946a7f5f9b97fc86731f063f353eca90b6b975
陈宇峰 <yufengcode@gmail.com>
2017-01-14 23:36:07 +0800

    格式化代码

--- public/javascripts/dao/connectDAO.js
+++ public/javascripts/dao/connectDAO.js
@@ -5,8 +5,8 @@ var mongoose = require('mongoose');
 var configDAO = require('./configDAO');
 
 module.exports.connect = function () {
-     var db = mongoose.connect(configDAO.mongodb);
-     //这里加载模型
+    var db = mongoose.connect(configDAO.mongodb);
+    //这里加载模型
     require('./model/customer.server.model');
     require('./model/merchant.server.model');
     require('./model/manager.server.model');

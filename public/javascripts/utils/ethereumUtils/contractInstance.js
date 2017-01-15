d7c7004718cea0d25edb0f929ffa8305fcd11e18
陈宇峰 <yufengcode@gmail.com>
2017-01-15 21:11:12 +0800

    log优化

--- public/javascripts/utils/ethereumUtils/contractInstance.js
+++ public/javascripts/utils/ethereumUtils/contractInstance.js
@@ -1,16 +1,17 @@
 var fs = require('fs');
 var web3Instance = require('./web3Instance');
+var LOG = require('../commonUtils/LOG');
 
 //web3初始化
 var web3 = web3Instance.web3;
 
 //获得合约实例
 fs.readFile("./contract/abiString.txt", function (error, result) {
-    console.log(result.toString());
+    console.log(LOG.ETH_ABI_FILE + ":" + result.toString());
     var abiString = result.toString();
 
     fs.readFile("./contract/contractAddress.txt", function (error, result) {
-        console.log(result.toString());
+        console.log(LOG.ETH_CONTRACT_ADDRESS + ":" + result.toString());
         var contractAddress = result.toString();
 
         global.contractInstance = web3.eth.contract(JSON.parse(abiString)).at(contractAddress);

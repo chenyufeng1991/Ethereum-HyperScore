195a4950c58dd8c0b48fff861d9cec8a06072706
陈宇峰 <yufengcode@gmail.com>
2017-01-15 13:02:49 +0800

    完成config配置文件，关于数据库、Ethereum配置放入其中

---
+++ public/javascripts/config/config.js
@@ -0,0 +1,13 @@
+/**
+ * Created by chenyufeng on 15/01/2017.
+ */
+
+/**
+ * 整个项目的配置文件
+ *
+ * @type {{mongodb: string, clientUrl: string}}
+ */
+module.exports = {
+    mongodb: "mongodb://localhost/HyperScore",
+    clientUrl: "http://localhost:8545"
+};
--- public/javascripts/utils/ethereumUtils/web3Instance.js
+++ public/javascripts/utils/ethereumUtils/web3Instance.js
@@ -1,4 +1,5 @@
 var Web3 = require('web3');
+var config = require('../../config/config');
 
 //web3初始化
 var web3;
@@ -6,7 +7,7 @@ if (typeof web3 !== 'undefined') {
     web3 = new Web3(web3.currentProvider);
 }
 else {
-    web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    web3 = new Web3(new Web3.providers.HttpProvider(config.clientUrl));
 }
 
 module.exports.web3 = web3;

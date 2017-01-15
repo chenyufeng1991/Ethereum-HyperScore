2dbcca7ce67938f430e159d235ed537cad4ab8f1
陈宇峰 <yufengcode@gmail.com>
2017-01-15 11:39:44 +0800

    log->error优化

--- routes/manager/managerDetail.js
+++ routes/manager/managerDetail.js
@@ -54,7 +54,7 @@ module.exports.query = function (req, res) {
             res.end();
         }
         else {
-            console.log("发生错误：" + error);
+            console.error("发生错误：" + error);
             var response = {
                 code: 1,
                 error: error.toString(),

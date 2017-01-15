ef620c24e67f13bf9295dac3cf242a9acd27af86
陈宇峰 <yufengcode@gmail.com>
2017-01-15 11:44:07 +0800

    log->error打印优化

--- routes/score/settleScore.js
+++ routes/score/settleScore.js
@@ -52,7 +52,7 @@ module.exports.settle = function (req, res) {
             });
         }
         else {
-            console.log("发生错误：" + error);
+            console.error("发生错误：" + error);
             var response = {
                 code: 1,
                 error: error.toString(),

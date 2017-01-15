e383c15233946ed1cf57276ee0becb2a7d3aad4d
陈宇峰 <yufengcode@gmail.com>
2017-01-15 15:35:32 +0800

    LOG日志管理优化

--- public/javascripts/utils/commonUtils/LOG.js
+++ public/javascripts/utils/commonUtils/LOG.js
@@ -8,6 +8,8 @@ module.exports = {
     ETH_ABI_FILE: "abi文件",
     ETH_BINARY_CODE: "code字节码",
     ETH_TRANSACTION_HASH: "交易hash",
-    ETH_CONTRACT_ADDRESS: "合约地址"
-
+    ETH_CONTRACT_ADDRESS: "合约地址",
+    ETH_GETH_ACCOUNT_RESULT: "geth创建账户结果",
+    ETH_GETH_CREATE_ACCOUNT_SUCCESS: "geth创建账户成功",
+    ETH_GETH_CREATE_ACCOUNT_FAILED: "geth创建账户失败"
 };

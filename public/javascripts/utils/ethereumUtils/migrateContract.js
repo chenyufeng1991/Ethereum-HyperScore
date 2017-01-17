1c3cb1f2658c9f59f0c475e0d3632117b596064e
陈宇峰 <yufengcode@gmail.com>
2017-01-17 17:21:11 +0800

    完成合约迁移脚本

--- public/javascripts/utils/ethereumUtils/migrateContract.js
+++ public/javascripts/utils/ethereumUtils/migrateContract.js
@@ -1,16 +1,9 @@
 /**
- * 该文件为合约的自动化部署脚本，本次要部署新合约的时候，执行该文件即可。
- * 部署脚本不应该在服务端被调用，否则每次启动服务器都要部署新合约，不符合实际应用。
+ * Created by chenyufeng on 17/01/2017.
  */
 var fs = require('fs');
 var path = require('path');
 var LOG = require('../commonUtils/LOG');
-
-//log4js
-var log4js = require('log4js');
-var log4jsConfig = require('../../config/log4jsConfig');
-log4js.configure(log4jsConfig.config('../../../log/deployContract.log'));
-var logger = log4jsConfig.logger;
 
 //web3
 var judgeNodeType = require('./judgeNodeType');
@@ -19,12 +12,12 @@ var web3 = web3Instance.web3;
 
 //读取合约
 //如果由其他文件来调用该模块，则fs中写入的路径应该是相对于调用者来的，而不是该文件
-fs.readFile("../../../contract/Score.sol", function (error, result) {
+fs.readFile("../../../../contract/new/new_Score.sol", function (error, result) {
     console.log(LOG.ETH_CONTRACT + ":" + result.toString());
     //编译合约
     var compileJSON = JSON.stringify(web3.eth.compile.solidity(result.toString()), null, " "); //格式化输出
     console.log(LOG.ETH_COMPILED_CONTRACT + ":" + compileJSON);
-    fs.writeFile("../../../contract/compileJSON.txt", compileJSON); //写入文件
+    fs.writeFile("../../../../contract/new/new_compileJSON.txt", compileJSON); //写入文件
 
     //获得abi文件
     var abiString;
@@ -37,7 +30,7 @@ fs.readFile("../../../contract/Score.sol", function (error, result) {
         abiString = JSON.stringify(web3.eth.compile.solidity(result.toString()).Score.info.abiDefinition, null, " ");
     }
     console.log(LOG.ETH_ABI_FILE + ":" + abiString);
-    fs.writeFile("../../../contract/abiString.txt", abiString);
+    fs.writeFile("../../../../contract/new/new_abiString.txt", abiString);
 
     //获得code字节码
     var codeString;
@@ -50,7 +43,7 @@ fs.readFile("../../../contract/Score.sol", function (error, result) {
         codeString = web3.eth.compile.solidity(result.toString()).Score.code;
     }
     console.log(LOG.ETH_BINARY_CODE + ":" + codeString);
-    fs.writeFile("../../../contract/codeString.txt", codeString);
+    fs.writeFile("../../../../contract/new/new_codeString.txt", codeString);
 
     //根据abi和bytecode部署合约;如果这里error，有可能是OOG造成的
     web3.eth.contract(JSON.parse(abiString)).new({
@@ -64,8 +57,8 @@ fs.readFile("../../../contract/Score.sol", function (error, result) {
         else {
             //获得部署的合约地址
             var contractAddress = contract.address;
-            console.log(LOG.ETH_CONTRACT_ADDRESS + ":" + contractAddress);
-            fs.writeFile("../../../contract/contractAddress.txt", contractAddress);
+            console.log("新合约地址" + ":" + contractAddress);
+            fs.writeFile("../../../../contract/new/new_contractAddress.txt", contractAddress);
 
             contract.setAge(8888, {from: web3.eth.coinbase}, function (error, result) {
                 console.log(result);
@@ -78,6 +71,28 @@ fs.readFile("../../../contract/Score.sol", function (error, result) {
                 console.log(event.args.age);
                 eventSetAge.stopWatching();
             });
+
+            initOldContract(contract);
         }
     });
 });
+
+function initOldContract(newContract) {
+
+    fs.readFile("../../../../contract/abiString.txt", function (error, result) {
+        console.log(LOG.ETH_ABI_FILE + ":" + result.toString());
+        var abiString = result.toString();
+
+        fs.readFile("../../../../contract/contractAddress.txt", function (error, result) {
+            console.log("旧合约地址" + ":" + result.toString());
+            var contractAddress = result.toString();
+            var oldContract = web3.eth.contract(JSON.parse(abiString)).at(contractAddress);
+
+            startMigrate(oldContract, newContract);
+        });
+    });
+}
+
+function startMigrate(oldContract, newContract) {
+    console.log("chenyufeng：" + oldContract.address + "    " + newContract.address);
+}

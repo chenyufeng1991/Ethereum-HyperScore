b3010a5898b5f7dab9d3f3fbb703e2172abf563f
陈宇峰 <yufengcode@gmail.com>
2017-01-17 22:56:26 +0800

    删除无用合约方法getTransactionDetail,优化代码

--- routes/transaction/transactionDetail.js
+++ routes/transaction/transactionDetail.js
@@ -34,16 +34,16 @@ module.exports.query = function (req, res) {
 
     console.log(LOG.ETH_TRANSACTION_HASH + ":" + txHash);
 
-    global.contractInstance.getTransactionDetail(txHash, function (error, result) {
+    global.contractInstance.getTransaction(txHash, function (error, result) {
         if (!error) {
-            console.log(LOG.CS_TX_STATE + ":" + result[0] + LOG.CS_SENDER_PHONE + ":" + commonUtils.hexCharCodeToStr(result[1]) +
-                LOG.CS_RECEIVER_PHONE + ":" + commonUtils.hexCharCodeToStr(result[2]) + LOG.CS_SCORE_AMOUNT + ":" + result[3]);
+            console.log(LOG.CS_TX_STATE + ":" + result[1] + LOG.CS_SENDER_PHONE + ":" + commonUtils.hexCharCodeToStr(result[2]) +
+                LOG.CS_RECEIVER_PHONE + ":" + commonUtils.hexCharCodeToStr(result[3]) + LOG.CS_SCORE_AMOUNT + ":" + result[4]);
 
             var obj = {
-                txType: result[0],
-                sender: commonUtils.hexCharCodeToStr(result[1]),
-                receiver: commonUtils.hexCharCodeToStr(result[2]),
-                score: result[3]
+                txType: result[1],
+                sender: commonUtils.hexCharCodeToStr(result[2]),
+                receiver: commonUtils.hexCharCodeToStr(result[3]),
+                score: result[4]
             };
 
             var response = {

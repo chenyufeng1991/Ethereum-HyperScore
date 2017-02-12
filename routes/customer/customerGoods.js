b91bc09147cfa4e0f9d57bf0c8c89a94df2cd3d8
陈宇峰 <yufengcode@gmail.com>
2017-02-13 00:09:09 +0800

    客户查询所有商品接口实现分页

--- routes/customer/customerGoods.js
+++ routes/customer/customerGoods.js
@@ -19,20 +19,33 @@ var LOG = require('../../public/javascripts/utils/commonUtils/LOG');
  */
 module.exports.query = function (req, res) {
     var phone = req.query.phone;
-    console.log(LOG.CS_PHONE + ":" + phone);
+    var pageNum = req.query.pageNum;
+    console.log(LOG.CS_PHONE + ":" + phone + "；第几页：" + pageNum);
 
     global.contractInstance.getGoodsByCustomer(phone, function (error, result) {
         if (!error) {
             console.log("返回的商品数组长度：" + result.length);
             var goodArray = [];
-            for (var i = 0; i < result.length; i++) {
+            var totalPage = 0;
+            if(result.length % 10 == 0) {
+                totalPage = result.length / 10;
+            }
+            else {
+                totalPage = parseInt(result.length / 10) + 1;
+            }
+            pageNum--;
+            for (var i = 10 * pageNum; i < result.length && i < 10 * pageNum + 10; i++) {
                 goodArray.push(commonUtils.hexCharCodeToStr(result[i]));
             }
             console.log("返回的商品数组：" + goodArray);
+            var obj = {
+                goodArray: goodArray,
+                totalPage: totalPage
+            };
             var response = {
                 code: 0,
                 error: "",
-                result: goodArray,
+                result: obj,
                 txInfo: "",
                 requestUrl: req.originalUrl
             };
